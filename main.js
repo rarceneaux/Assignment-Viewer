8607765fff82c29ca96cdd98b62974e9bfe42f9b
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-08-26 23:46:51 -0500

    added css and format card

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('linked');
+const assignment = () => {[
+    {
+      title: 'Product Cards',
+      dueDate: 'September 3, 2019',
+      topic:'HTML & CSS',
+      notes:'Style cards and print to the DOM',
+      assignmentUrl:'https://www.nfl.com'
+    },
+    {
+      title: 'NFL',
+      dueDate: 'September 10, 2019',
+      topic:'Javascript',
+      notes:'Loop over array of teams and print the contents to DOM',
+      assignmentUrl:'https://www.nba.com'
+    },
+    {
+      title: 'React',
+      dueDate: 'September 17, 2019',
+      topic:'Set up files to accept data',
+      notes:'This is a test and only a test. Please Disregard',
+      assignmentUrl:'https://www.nashville.gov'
+    },
+    {
+      title: 'CSS',
+      dueDate: 'September 24, 2019',
+      topic:'CSS 101 Workshop',
+      notes:'High overview of the world of styling ',
+      assignmentUrl:'https://www.google.com'
+    },
+    {
+      title: 'Flexbox',
+      dueDate: 'September 31, 2019',
+      topic:'What is Flexbox?',
+      notes:'If you loved CSS then Flexbox will add more to your experience',
+      assignmentUrl:'https://fristartmuseum.org'
+    }
+  ]};
